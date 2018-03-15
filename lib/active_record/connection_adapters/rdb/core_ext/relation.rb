1e9772c4756a94a75a5fb64f8b75692abd6d12bc
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-03-15 11:42:35 +0300

    Update all when using handwritten sql-s;

--- lib/active_record/connection_adapters/rdb/core_ext/relation.rb
+++ lib/active_record/connection_adapters/rdb/core_ext/relation.rb
@@ -10,11 +10,15 @@ ActiveRecord::Relation.class_eval do
       stmt.table(table)
       value_binds = []
       arel_values = []
-      updates.each do |column, value|
-        value_binds.push ActiveRecord::Relation::QueryAttribute.new(column, value, @klass.type_for_attribute(column))
-        arel_values.push Arel::Nodes::Assignment.new(Arel::Nodes::UnqualifiedColumn.new(Arel::Attributes::Attribute.new(table, column)), Arel::Nodes::BindParam.new)
+      if !updates.empty? && !updates.first.nil? && !updates.first.is_a?(String)
+        updates.each do |column, value|
+          value_binds.push ActiveRecord::Relation::QueryAttribute.new(column, value, @klass.type_for_attribute(column))
+          arel_values.push Arel::Nodes::Assignment.new(Arel::Nodes::UnqualifiedColumn.new(Arel::Attributes::Attribute.new(table, column)), Arel::Nodes::BindParam.new)
+        end
+        stmt.ast.values = arel_values
+      else
+        stmt.set Arel.sql(@klass.send(:sanitize_sql_for_assignment, updates))
       end
-      stmt.ast.values = arel_values
 
       if has_join_values?
         @klass.connection.join_to_update(stmt, arel, arel_attribute(primary_key))
