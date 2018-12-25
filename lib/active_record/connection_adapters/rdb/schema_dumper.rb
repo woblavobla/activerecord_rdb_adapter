d298e6968492dd61088294b7de14bd980ffef967
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-12-25 15:50:39 +0300

    #25065 Schema dump minor;

--- lib/active_record/connection_adapters/rdb/schema_dumper.rb
+++ lib/active_record/connection_adapters/rdb/schema_dumper.rb
@@ -9,6 +9,14 @@ module ActiveRecord
           spec.delete(:auto_increment) if column.type == :integer && column.auto_increment?
           spec
         end
+
+        def schema_type(column)
+          if column.bigint?
+            :bigint
+          else
+            column.type.type
+          end
+        end
       end
     end
   end
